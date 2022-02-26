39c8a819d51e5a95d90f95dc7c453c0c8d4f7d81
gtwene <gtwene001@st.ug.edu.gh>
2022-02-26 15:23:23 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var onClickRedbox = document.getElementById("redbox");
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+span.addEventListener("click", function () {
+  anime({
+    targets: "div.box.red",
+    left: "1000px",
+    backgroundColor: "#C23214 ",
+    borderRadius: ["0%", "100"],
+    easing: "easeInOutQuad",
+  });
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
